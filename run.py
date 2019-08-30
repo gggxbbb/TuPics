5bc26d9742cf45f8a2374fab52070a49f04cc4a7
gggxbbb <2331490629@qq.com>
2019-08-31 00:03:33 +0800

    bug fixed

--- run.py
+++ run.py
@@ -1,13 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 import os
 import json
+import pytz  
+import datetime  
+import time
 from urllib import request
-from email.utils import formatdate
 from jinja2 import Template
-
-output_pics={}
-
-output_pics['info'] = {}
-output_pics['info']['start'] = formatdate(None, usegmt=True)
 
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
@@ -16,6 +15,15 @@ def getJson(url):
     req = request.Request(url, headers=header)
     data = request.urlopen(req).read().decode('utf-8')
     return json.loads(data)
+
+def getTime():
+    beijing_time = datetime.datetime.now(pytz.timezone('PRC'))
+    return beijing_time.strftime('%Y-%m-%d %H:%M:%S')
+
+
+output_pics={}
+output_pics['info'] = {}
+output_pics['info']['start'] = getTime()
 
 if not os.path.isdir('build'):
     os.mkdir('build')
@@ -33,7 +41,7 @@ def buildOne(pic):
         f.close()
 
 # sort_all
-output_pics['info']['sort'] = {'all':{'start':formatdate(None, usegmt=True)}}
+output_pics['info']['sort'] = {'all':{'start':getTime()}}
 sort = getJson('https://v2.api.dailypics.cn/sort')['result']
 output_pics['sort'] = sort
 output_pics['sort_map'] = {}
@@ -44,19 +52,19 @@ for k in sort:
     output_pics['sort_map'][k['TID']] = k
     output_pics['archive'][k['TID']]=[]
     output_pics['info']['sort'][k['TID']] = {}
-output_pics['info']['sort']['all']['end']=formatdate(None, usegmt=True)
+output_pics['info']['sort']['all']['end']=getTime()
 
 # today
 output_pics['info']['today'] = {}
-output_pics['info']['today']['start']= formatdate(None, usegmt=True)
+output_pics['info']['today']['start']= getTime()
 today = getJson('https://v2.api.dailypics.cn/today')
 output_pics['today'] = today
-output_pics['info']['today']['end']= formatdate(None, usegmt=True)
+output_pics['info']['today']['end']= getTime()
 
 # sort_one
 for k in sort:
     print(k)
-    output_pics['info']['sort'][k['TID']]['start'] = formatdate(None, usegmt=True)
+    output_pics['info']['sort'][k['TID']]['start'] = getTime()
     first_page = getJson('https://v2.api.dailypics.cn/list/?page=1&size=15&sort=%s' % k['TID'])
     max_page = first_page['maxpage']
     print(max_page)
@@ -68,11 +76,11 @@ for k in sort:
         print(page)
         for pic in this_page:
             output_pics['archive'][pic['TID']].append(pic)
-    output_pics['info']['sort'][k['TID']]['end'] = formatdate(None, usegmt=True)
+    output_pics['info']['sort'][k['TID']]['end'] = getTime()
 
 # output
 
-output_pics['info']['end']= formatdate(None, usegmt=True)
+output_pics['info']['end']= getTime()
 
 for v in today:
     for p in today:
