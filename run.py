f2d473e8cdc058c34500ea00fa4ae546e81c4a82
gggxbbb <2331490629@qq.com>
2019-08-30 11:07:43 +0800

    bug fixed

--- run.py
+++ run.py
@@ -20,24 +20,35 @@ def getJson(url):
 if not os.path.isdir('build'):
     os.mkdir('build')
 
+with open('pages/detail.html','r',encoding='utf-8') as f:
+    datail_page = Template(f.read())
+    f.close()
+
+def buildOne(pic):
+    with open('build/%s.html'%pic['PID'],'w',encoding='utf-8') as f:
+        f.write(datail_page.render(pic=pic,sort=output_pics['sort_map'][pic['TID']]))
+        f.close()
+
+# sort_all
+output_pics['info']['sort'] = {'all':{'start':formatdate(None, usegmt=True)}}
+sort = getJson('https://v2.api.dailypics.cn/sort')['result']
+output_pics['sort'] = sort
+output_pics['sort_map'] = {}
+output_pics['archive']={}
+print(sort)
+for k in sort:
+    print(k)
+    output_pics['sort_map'][k['TID']] = k
+    output_pics['archive'][k['TID']]=[]
+    output_pics['info']['sort'][k['TID']] = {}
+output_pics['info']['sort']['all']['end']=formatdate(None, usegmt=True)
+
 # today
 output_pics['info']['today'] = {}
 output_pics['info']['today']['start']= formatdate(None, usegmt=True)
 today = getJson('https://v2.api.dailypics.cn/today')
 output_pics['today'] = today
 output_pics['info']['today']['end']= formatdate(None, usegmt=True)
-
-# sort_all
-output_pics['info']['sort'] = {'all':{'start':formatdate(None, usegmt=True)}}
-sort = getJson('https://v2.api.dailypics.cn/sort')['result']
-output_pics['sort'] = sort
-output_pics['archive']={}
-print(sort)
-for k in sort:
-    print(k)
-    output_pics['archive'][k['TID']]=[]
-    output_pics['info']['sort'][k['TID']] = {}
-output_pics['info']['sort']['all']['end']=formatdate(None, usegmt=True)
 
 # sort_one
 for k in sort:
@@ -56,12 +67,17 @@ for k in sort:
             output_pics['archive'][pic['TID']].append(pic)
     output_pics['info']['sort'][k['TID']]['end'] = formatdate(None, usegmt=True)
 
-output_pics['info']['end']= formatdate(None, usegmt=True)
-
 # output
-with open('build/all.json','w',encoding='utf-8') as f:
-    f.write(json.dumps(output_pics))
-    f.close()
+
+for v in today:
+    for p in today:
+        print(p['PID'])
+        buildOne(p)
+
+for v in sort:
+    for p in output_pics['archive'][v['TID']]:
+        print(p['PID'])
+        buildOne(p)
 
 with open('pages/home.html','r',encoding='utf-8') as f:
     index_page = Template(f.read())
@@ -69,4 +85,10 @@ with open('pages/home.html','r',encoding='utf-8') as f:
 
 with open('build/index.html','w',encoding='utf-8') as f:
     f.write(index_page.render(pics=output_pics))
+    f.close()
+
+output_pics['info']['end']= formatdate(None, usegmt=True)
+
+with open('build/all.json','w',encoding='utf-8') as f:
+    f.write(json.dumps(output_pics))
     f.close()
