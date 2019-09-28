5e5001fda0116d8c24e4091a6199c5059b6c4908
gggxbbb <2331490629@qq.com>
2019-09-28 18:49:05 +0800

    add sth

---
+++ excel.py
@@ -0,0 +1,73 @@
+import xlwt
+import json
+
+input_pics = json.loads(open('build/all.json', 'r', encoding='utf-8').read())
+
+output_pics = []
+
+for pic in input_pics['today']:
+    if not pic['TID'] in [v['TID'] for v in input_pics['not_updated']['sort']]:
+        output_pics.append(pic)
+    output_pics += input_pics['archive'][pic['TID']]
+
+output_table = []
+output_table.append([
+    '标题', 
+    'ID', 
+    '类别', 
+    '日期',
+    '介绍', 
+    '上传者', 
+    '地址', 
+    '原始地址', 
+    '主题色', 
+    '文本建议色',
+    '永久地址'
+    ])
+for pic in output_pics:
+    output_table.append([
+        pic['p_title'], 
+        pic['PID'], 
+        input_pics['sort_map'][pic['TID']]['T_NAME'], 
+        pic['p_date'],
+        pic['p_content'], 
+        pic['username'], 
+        pic['local_url'], 
+        pic['p_link'], 
+        pic['theme_color'], 
+        pic['text_color'],
+        'https://www.dailypics.cn/member/id/'+pic['PID']
+        ])
+
+
+output_book = xlwt.Workbook()
+
+output_table_info = output_book.add_sheet('信息')
+
+output_table_info.write(0,1,'开始时间')
+output_table_info.write(0,2,'结束时间')
+
+output_table_info.write(1,0,'总体')
+output_table_info.write(1,1,input_pics['info']['start'])
+output_table_info.write(1,2,input_pics['info']['end'])
+
+output_table_info.write(2,0,'今日')
+output_table_info.write(2,1,input_pics['info']['today']['start'])
+output_table_info.write(2,2,input_pics['info']['today']['end'])
+
+output_table_info.write(3,0,'归档')
+output_table_info.write(3,1,input_pics['info']['sort']['all']['start'])
+output_table_info.write(3,2,input_pics['info']['sort']['all']['end'])
+
+output_table_pic = output_book.add_sheet('归档')
+
+row = 0
+for pic in output_table:
+    print(row)
+    col = 0
+    for v in pic:
+        output_table_pic.write(row, col, v)
+        col += 1
+    row += 1
+
+output_book.save('build/Tujian.xls')
--- run.py
+++ run.py
@@ -17,6 +17,8 @@ ua = [
 ]
 
 # 判断是否为 Windows
+
+
 def ifWindows():
     if(platform.system() == 'Windows'):
         return True
@@ -24,71 +26,85 @@ def ifWindows():
         return False
 
 # 获取 JOSN 数据
+
+
 def getJson(url):
-    req = request.Request(url, headers={'User-Agent':ua[random.randint(0,len(ua)-1)]})
+    req = request.Request(
+        url, headers={'User-Agent': ua[random.randint(0, len(ua)-1)]})
     data = request.urlopen(req).read().decode('utf-8')
     return json.loads(data)
 
 # 获取格式化的北京时间
+
+
 def getTime():
     beijing_time = datetime.datetime.now(pytz.timezone('PRC'))
     return beijing_time.strftime('%Y-%m-%d %H:%M:%S')
 
 # 下载图片
+
+
 def download(pic):
-    file_path = 'build/%s'%pic['file_name']
-    file_lite = 'build/%s-lite.jpg'%pic['PID']
+    file_path = 'build/%s' % pic['file_name']
+    file_lite = 'build/%s-lite.jpg' % pic['PID']
     if ifWindows():
-        print('%s passing'%file_path)
+        print('%s passing' % file_path)
         return 0
     if os.path.isfile(file_path):
-        print('%s 已存在'%file_path)
+        print('%s 已存在' % file_path)
     else:
         print('download')
-        req = request.Request(pic['local_url']+'?p=0', headers={'User-Agent':ua[random.randint(0,len(ua)-1)]})
+        req = request.Request(
+            pic['local_url']+'?p=0', headers={'User-Agent': ua[random.randint(0, len(ua)-1)]})
         data = request.urlopen(req).read()
-        with open(file_path,'wb') as f:
+        with open(file_path, 'wb') as f:
             f.write(data)
             f.close()
     if os.path.isfile(file_lite):
-        print('%s 已存在'%file_lite)
+        print('%s 已存在' % file_lite)
     else:
-        req2 = request.Request(pic['local_url']+'?f=jpg', headers={'User-Agent':ua[random.randint(0,len(ua)-1)]})
+        req2 = request.Request(
+            pic['local_url']+'?f=jpg', headers={'User-Agent': ua[random.randint(0, len(ua)-1)]})
         data2 = request.urlopen(req2).read()
         print('-lite')
-        with open(file_lite,'wb') as f:
+        with open(file_lite, 'wb') as f:
             f.write(data2)
             f.close()
 
 # 计算长宽比
-def getAsp(height,width):
-    f = Fraction(width,height)
-    return '%s:%s'%(f.numerator,f.denominator)
+
+
+def getAsp(height, width):
+    f = Fraction(width, height)
+    return '%s:%s' % (f.numerator, f.denominator)
+
 
 def putUser(pic):
     if not (pic['username'] in output_pics['username']):
         output_pics['username'].append(pic['username'])
         output_pics['users'][pic['username']] = []
-    if not pic['PID'] in [ v['PID'] for v in output_pics['users'][pic['username']] ]:
+    if not pic['PID'] in [v['PID'] for v in output_pics['users'][pic['username']]]:
         output_pics['users'][pic['username']].append(pic)
+
 
 def putAsp(pic):
     if not (pic['aspect_ratio'] in output_pics['aspect_ratio']):
         output_pics['asp'].append(pic['aspect_ratio'])
         output_pics['aspect_ratio'][pic['aspect_ratio']] = []
-    if not pic['PID'] in [ v['PID'] for v in output_pics['aspect_ratio'][pic['aspect_ratio']] ]:
+    if not pic['PID'] in [v['PID'] for v in output_pics['aspect_ratio'][pic['aspect_ratio']]]:
         output_pics['aspect_ratio'][pic['aspect_ratio']].append(pic)
+
 
 def putDate(pic):
     if not (pic['p_date'] in output_pics['dates']):
         output_pics['dates'].append(pic['p_date'])
         output_pics['date'][pic['p_date']] = []
-    if not pic['PID'] in [ v['PID'] for v in output_pics['date'][pic['p_date']] ]:
+    if not pic['PID'] in [v['PID'] for v in output_pics['date'][pic['p_date']]]:
         output_pics['date'][pic['p_date']].append(pic)
 
 
 # 初始化字典
-output_pics={}
+output_pics = {}
 output_pics['info'] = {}
 output_pics['info']['start'] = getTime()
 output_pics['username'] = []
@@ -106,143 +122,154 @@ if not os.path.isdir('build'):
     os.mkdir('build')
 
 # 加载 Detail 页面模板
-with open('pages/detail.html','r',encoding='utf-8') as f:
+with open('pages/detail.html', 'r', encoding='utf-8') as f:
     datail_page = Template(f.read())
     f.close()
-with open('pages/archive.html','r',encoding='utf-8') as f:
+with open('pages/archive.html', 'r', encoding='utf-8') as f:
     archive_page = Template(f.read())
     f.close()
 
 # 用于构建单张图片的 Detail 页面
+
+
 def buildOne(pic):
-    with open('build/%s.html'%pic['PID'],'w',encoding='utf-8') as f:
-        f.write(datail_page.render(pic=pic,sort=output_pics['sort_map'][pic['TID']]))
+    with open('build/%s.html' % pic['PID'], 'w', encoding='utf-8') as f:
+        f.write(datail_page.render(
+            pic=pic, sort=output_pics['sort_map'][pic['TID']]))
         f.close()
-    with open('build/%s.json'%pic['PID'],'w',encoding='utf-8') as f:
+    with open('build/%s.json' % pic['PID'], 'w', encoding='utf-8') as f:
         f.write(json.dumps(pic))
         f.close()
-def buildArchive(pics,title,name):
-    with open('build/%s.html'%name,'w',encoding='utf-8') as f:
-        f.write(archive_page.render(pics=pics,sort=output_pics['sort_map'],title=title))
-        f.close()
 
+
+def buildArchive(pics, title, name):
+    with open('build/%s.html' % name, 'w', encoding='utf-8') as f:
+        f.write(archive_page.render(
+            pics=pics, sort=output_pics['sort_map'], title=title))
+        f.close()
 
 
 # 获取分类
 print('sorts')
-## 记录开始时间
-output_pics['info']['sort'] = {'all':{'start':getTime()}}
-## 获取分类数据
+# 记录开始时间
+output_pics['info']['sort'] = {'all': {'start': getTime()}}
+# 获取分类数据
 sort = getJson('https://api.dpic.dev/sort')['result']
-## 将分类数据存入字典
+# 将分类数据存入字典
 output_pics['sort'] = sort
-## 初始化存储
+# 初始化存储
 output_pics['sort_map'] = {}
-output_pics['archive']={}
-## 打个输出，以免看着心慌
+output_pics['archive'] = {}
+# 打个输出，以免看着心慌
 print(sort)
-## 遍历分类
+# 遍历分类
 for v in sort:
-    ### 打个输出，以免看着心慌
+    # 打个输出，以免看着心慌
     print(v)
-    ### 将 List 变为 Dict
+    # 将 List 变为 Dict
     output_pics['sort_map'][v['TID']] = v
-    ### 初始化各分类归档
-    output_pics['archive'][v['TID']]=[]
+    # 初始化各分类归档
+    output_pics['archive'][v['TID']] = []
     output_pics['info']['sort'][v['TID']] = {}
-## 记录结束时间
-output_pics['info']['sort']['all']['end']=getTime()
+# 记录结束时间
+output_pics['info']['sort']['all']['end'] = getTime()
 
 
 # 获取今日
 print('today')
-## 记录开始时间
+# 记录开始时间
 output_pics['info']['today'] = {}
-output_pics['info']['today']['start']= getTime()
-## 获取今日
+output_pics['info']['today']['start'] = getTime()
+# 获取今日
 today = getJson('https://api.dpic.dev/today')
 output_pics['today'] = today
-## 处理今日
+# 处理今日
 for v in output_pics['today']:
     print(v['PID'])
-    v['mainland_url'] = v['local_url'].replace('img.dpic.dev','images.dailypics.cn')
-    v['aspect_ratio'] = getAsp(v['height'],v['width'])
-    v['info'] = getJson(v['local_url'].replace('dev/','dev/info?md5='))['info']
+    v['mainland_url'] = v['local_url'].replace(
+        'img.dpic.dev', 'images.dailypics.cn')
+    v['aspect_ratio'] = getAsp(v['height'], v['width'])
+    v['info'] = getJson(v['local_url'].replace(
+        'dev/', 'dev/info?md5='))['info']
     v['file_name'] = v['PID'] + '.' + v['info']['format'].lower()
     putAsp(v)
     putUser(v)
     putDate(v)
     download(v)
-## 记录结束时间
-output_pics['info']['today']['end']= getTime()
+# 记录结束时间
+output_pics['info']['today']['end'] = getTime()
 
 
 # 是否咕咕咕
 print('GuGuGu')
-## 初始化 List
+# 初始化 List
 GuGuGu = []
-## 遍历今日图片
+# 遍历今日图片
 for v in output_pics['today']:
-    ### 判断是否咕咕咕并存储
+    # 判断是否咕咕咕并存储
     if (v['p_date'] == date_today):
         v['if_today'] = True
     else:
         v['if_today'] = False
-        #### 记录咕咕咕的分类
+        # 记录咕咕咕的分类
         GuGuGu.append(output_pics['sort_map'][v['TID']])
-## 将咕咕咕的分类存入主 Dict
-output_pics['not_updated'] = {'sort':GuGuGu}
-## 输出可读的咕咕咕情况
+# 将咕咕咕的分类存入主 Dict
+output_pics['not_updated'] = {'sort': GuGuGu}
+# 输出可读的咕咕咕情况
 print(GuGuGu)
 if (not (len(GuGuGu) == 0)):
-    ### 如果咕咕咕输出咕咕咕的分类
+    # 如果咕咕咕输出咕咕咕的分类
     GuGuGu_srt = ','.join([v['T_NAME'] for v in GuGuGu]) + '没有更新'
 else:
     GuGuGu_srt = '所有分类均已更新'
-## 将可读的咕咕咕情况存入主 Dict
+# 将可读的咕咕咕情况存入主 Dict
 output_pics['not_updated']['info'] = GuGuGu_srt
 
 # 单个分类
 print('sort')
-## 遍历所有分类
+# 遍历所有分类
 for v in sort:
-    ### 打个输出，以免看着心慌
+    # 打个输出，以免看着心慌
     print(v['TID'])
-    ### 记录开始时间
+    # 记录开始时间
     output_pics['info']['sort'][v['TID']]['start'] = getTime()
-    ### 获取第一页和总页数
-    first_page = getJson('https://api.dpic.dev/list/?page=1&size=15&sort=%s' % v['TID'])
+    # 获取第一页和总页数
+    first_page = getJson(
+        'https://api.dpic.dev/list/?page=1&size=15&sort=%s' % v['TID'])
     max_page = first_page['maxpage']
-    ### 打个输出，以免看着心慌
+    # 打个输出，以免看着心慌
     print(max_page)
-    ### 遍历结果，存入主 Dict
+    # 遍历结果，存入主 Dict
     for pic in first_page['result']:
         output_pics['archive'][pic['TID']].append(pic)
-    ### 循环获取之后的个页
+    # 循环获取之后的个页
     for p in range(1, int(max_page)):
         page = p+1
-        this_page = getJson('https://api.dpic.dev/list/?page=%s&size=15&sort=%s' % (page, v['TID']))['result']
+        this_page = getJson(
+            'https://api.dpic.dev/list/?page=%s&size=15&sort=%s' % (page, v['TID']))['result']
         print(page)
         for pic in this_page:
             output_pics['archive'][pic['TID']].append(pic)
-    ### 记录结束时间
+    # 记录结束时间
     output_pics['info']['sort'][v['TID']]['end'] = getTime()
 
-## 处理归档
+# 处理归档
 for v in sort:
     for pic in output_pics['archive'][v['TID']]:
         print(pic['PID'])
-        pic['mainland_url'] = pic['local_url'].replace('img.dpic.dev','images.dailypics.cn')
-        pic['aspect_ratio'] = getAsp(pic['height'],pic['width'])
-        pic['info'] = getJson(pic['local_url'].replace('dev/','dev/info?md5='))['info']
+        pic['mainland_url'] = pic['local_url'].replace(
+            'img.dpic.dev', 'images.dailypics.cn')
+        pic['aspect_ratio'] = getAsp(pic['height'], pic['width'])
+        pic['info'] = getJson(pic['local_url'].replace(
+            'dev/', 'dev/info?md5='))['info']
         pic['file_name'] = pic['PID'] + '.' + pic['info']['format'].lower()
         putUser(pic)
         putAsp(pic)
         putDate(pic)
         download(pic)
 
-## 记录结束时间
-output_pics['info']['end']= getTime()
+# 记录结束时间
+output_pics['info']['end'] = getTime()
 
 print('output')
 
@@ -252,96 +279,98 @@ for p in today:
     buildOne(p)
 
 # 输出所有图片的 Detail 页面
-## 遍历所有分类
+# 遍历所有分类
 for v in sort:
-    ### 遍历归档
+    # 遍历归档
     for p in output_pics['archive'][v['TID']]:
         print(p['PID'])
         buildOne(p)
 
 # 输出主页
-## 加载模板
-with open('pages/home.html','r',encoding='utf-8') as f:
+# 加载模板
+with open('pages/home.html', 'r', encoding='utf-8') as f:
     index_page = Template(f.read())
     f.close()
-## 输出主页
-with open('build/index.html','w',encoding='utf-8') as f:
-    f.write(index_page.render(pics=output_pics,not_updated=GuGuGu_srt))
+# 输出主页
+with open('build/index.html', 'w', encoding='utf-8') as f:
+    f.write(index_page.render(pics=output_pics, not_updated=GuGuGu_srt))
     f.close()
 
 # 输出 JSON
-## 输出今日图片
-with open('build/today.json','w',encoding='utf-8') as f:
-    buildArchive(output_pics['today'],'今日','today')
+# 输出今日图片
+with open('build/today.json', 'w', encoding='utf-8') as f:
+    buildArchive(output_pics['today'], '今日', 'today')
     f.write(json.dumps(output_pics['today']))
     f.close()
-## 输出分类
-with open('build/sort.json','w',encoding='utf-8') as f:
+# 输出分类
+with open('build/sort.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(output_pics['sort']))
     f.close()
-## 输出转换后的分类
-with open('build/sort2.json','w',encoding='utf-8') as f:
+# 输出转换后的分类
+with open('build/sort2.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(output_pics['sort_map']))
     f.close()
-## 输出 Users
-with open('build/username.json','w',encoding='utf-8') as f:
+# 输出 Users
+with open('build/username.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(output_pics['username']))
     f.close()
-with open('build/user-all.json','w',encoding='utf-8') as f:
+with open('build/user-all.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(output_pics['users']))
     f.close()
 for v in output_pics['username']:
-    with open('build/user-%s.json'%v,'w',encoding='utf-8') as f:
-        buildArchive(output_pics['users'][v],v,'user-' + v)
+    with open('build/user-%s.json' % v, 'w', encoding='utf-8') as f:
+        buildArchive(output_pics['users'][v], v, 'user-' + v)
         f.write(json.dumps(output_pics['users'][v]))
         f.close()
 
-with open('build/date.json','w',encoding='utf-8') as f:
+with open('build/date.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(output_pics['dates']))
     f.close()
-with open('build/date-all.json','w',encoding='utf-8') as f:
+with open('build/date-all.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(output_pics['date']))
     f.close()
 for v in output_pics['date'].keys():
-    with open('build/date-%s.json'%v,'w',encoding='utf-8') as f:
-        buildArchive(output_pics['date'][v],v,'date-' + v)
+    with open('build/date-%s.json' % v, 'w', encoding='utf-8') as f:
+        buildArchive(output_pics['date'][v], v, 'date-' + v)
         f.write(json.dumps(output_pics['date'][v]))
         f.close()
-## 输出 纵横比
-with open('build/asp.json','w',encoding='utf-8') as f:
+# 输出 纵横比
+with open('build/asp.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(output_pics['asp']))
     f.close()
-with open('build/asp-all.json','w',encoding='utf-8') as f:
+with open('build/asp-all.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(output_pics['aspect_ratio']))
     f.close()
 for v in output_pics['aspect_ratio'].keys():
-    with open(('build/asp-%s.json'%v).replace(':','-'),'w',encoding='utf-8') as f:
-        buildArchive(output_pics['aspect_ratio'][v],v,('asp-' + v).replace(':','-'))
+    with open(('build/asp-%s.json' % v).replace(':', '-'), 'w', encoding='utf-8') as f:
+        buildArchive(output_pics['aspect_ratio'][v],
+                     v, ('asp-' + v).replace(':', '-'))
         f.write(json.dumps(output_pics['aspect_ratio'][v]))
         f.close()
-## 输出各分类归档
-with open('build/sort-all.json','w',encoding='utf-8') as f:
-        f.write(json.dumps(output_pics['archive']))
-        f.close()
+# 输出各分类归档
+with open('build/sort-all.json', 'w', encoding='utf-8') as f:
+    f.write(json.dumps(output_pics['archive']))
+    f.close()
 for v in sort:
-    #### 输出归档
-    with open('build/sort-%s.json'%v['TID'],'w',encoding='utf-8') as f:
-        buildArchive(output_pics['archive'][v['TID']],v['T_NAME'],'sort-' + v['TID'])
+    # 输出归档
+    with open('build/sort-%s.json' % v['TID'], 'w', encoding='utf-8') as f:
+        buildArchive(output_pics['archive'][v['TID']],
+                     v['T_NAME'], 'sort-' + v['TID'])
         f.write(json.dumps(output_pics['archive'][v['TID']]))
         f.close()
-## 输出时间
-with open('build/info.json','w',encoding='utf-8') as f:
+# 输出时间
+with open('build/info.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(output_pics['info']))
     f.close()
-## 输出咕咕咕情况
-with open('build/not_updated.json','w',encoding='utf-8') as f:
+# 输出咕咕咕情况
+with open('build/not_updated.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(output_pics['not_updated']))
     f.close()
-## 输出主 Dict
-with open('build/all.json','w',encoding='utf-8') as f:
+# 输出主 Dict
+with open('build/all.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(output_pics))
     f.close()
-## 输出主 CNAME
-with open('build/CNAME','w',encoding='utf-8') as f:
+# 输出主 CNAME
+with open('build/CNAME', 'w', encoding='utf-8') as f:
     f.write('tu.gggxbbb.tk')
     f.close()
